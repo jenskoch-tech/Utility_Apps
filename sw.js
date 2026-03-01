91614644d49277938df5eaa13f587bf3a2fc3a29
CH-Stallikon <jens.koch1@googlemail.com>
2026-03-01 14:39:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
-const CACHE_NAME="headachetrack-v3";
-const ASSETS=["/","/index.html","/manifest.json","/icon.png"];
+const CACHE_NAME = "headachetrack-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon.png"
+];
 
-self.addEventListener("install",e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))) });
-self.addEventListener("fetch",e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
